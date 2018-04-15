f3f3d2c34ec1793b00267455ab00bef28f6c6aec
David Leonard <d+git@adaptive-enterprises.com>
2018-04-15 16:52:41 +1000

    lib/utf8: improve comments

--- lib/utf8.h
+++ lib/utf8.h
@@ -7,23 +7,29 @@
 typedef unsigned int ucode;
 
 /**
- * A sanitized Unicode code point is a restriction into the subset
+ * A sanitized Unicode code point is one restricted to the subset
  * {U+1..U+D7FF,U+DC00..U+DCFF,U+E000..U+10FFFF}.
- * The codepoints U+DC00..U+DCFF are used to represent "invalid" bytes.
- * U+0 is excluded for use as a string sentinel.
  *
- * In code, this is designated with the type
- * <code>__SANITIZED ucode</code>.
+ * In code, this type is designated <code>__SANITIZED ucode</code>.
  *
- * This range restriction is slightly different to RFC 3629's UTF-8
- * (and http://www.unicode.org/glossary/#unicode_scalar_value) which is
- * {U+0..U+D7FF,U+E000..U+10FFFF}.
+ * On U+0: Note that __SANITIZED ucode excludes 0, which
+ * means that a UTF-8 encoding of sanitized ucodes (yielding "sanitized
+ * UTF-8") can always become a valid C string. Also, a function
+ * returning a __SANITIZED ucode can use 0 to indicate exhaustion or
+ * an error.
+ *
+ * The sanitized range is <em>similar but different</em> to the "Unicode
+ * Scalar Value" (USV) as used in RFC 3629's UTF-8 and defined by
+ * http://www.unicode.org/glossary/#unicode_scalar_value as the range
+ * USV = {U+0..U+D7FF,U+E000..U+10FFFF}.
  * @see #IS_UTF8_SAFE() for a standard USV check.
  *
- * Sanitized codepoints transcode losslessly with "dirty UTF-8",
- * where codepoints from the U+DC00..U+DCFF range are mapped directly
- * to invalid bytes in the encoding. In particular, UTF-8 0x00 encodes
- * the sanitized value U+DC00.
+ * The __SANITIZED values U+DC00..U+DCFF are reserved for
+ * holding "dirty" bytes from an unsanitary UTF-8 source.
+ * Dirty bytes are those forming non-canonical UTF-8, or
+ * forming canonical UTF-8 that encodes outside of USV-{U+0}.
+ * Each dirty byte is individually mapped into {U+DC00..U+DCFF}.
+ * A reverse process recovers the dirty UTF-8 input exactly.
  *
  * The lossless or invalid-byte-preserving translation technique is due to
  * Marcus Kuhn ("Substituting malformed UTF-8 sequences in a decoder", 2000).
@@ -44,8 +50,7 @@ typedef unsigned int ucode;
 size_t get_utf8_raw_bounded(const char *p, const char *p_end,
 				ucode *u_return);
 size_t put_utf8_raw(ucode u, void *buf, int bufsz);
-__SANITIZED ucode
-       get_utf8_sanitized(const char **p_ptr);
+__SANITIZED ucode get_utf8_sanitized(const char **p_ptr);
 size_t put_sanitized_utf8(__SANITIZED ucode u, void *buf, int bufsz);
 
 #endif /* REDJSON_UTF8_H */
